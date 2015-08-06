d2421ca758a08634ec31c794abe4313c7b570177
1Mystery <misterX_2010@list.ru>
2015-08-06 15:05:00 +0300

    Added new classes

---
+++ ConsoleApplication/Dimentions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Dimentions
+    {
+        public Dimentions(int dimentionX, int dimentionY)
+        {
+            this.RowStart = 0;
+            this.RowEnd = dimentionX - 1;
+            this.ColumnStart = 0;
+            this.ColumnEnd = dimentionY - 1;
+        }
+        public int RowStart { get; set; }
+        public int RowEnd { get; set; }
+        public int ColumnStart { get; set; }
+        public int ColumnEnd { get; set; }
+    }
+}
---
+++ ConsoleApplication/Style.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Style
+    {
+        public void ApplyConsoleStyle()
+        {
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+        }
+    }
+}
